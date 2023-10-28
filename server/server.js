3d066c54eff93e48204cd8245413abe9a3249d6d
servet-isikli <servetisikli@yahoo.com>
2023-10-28 21:12:24 +0200

    edit

--- server/server.js
+++ server/server.js
@@ -3,17 +3,6 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-  );
-  next();
-});
-
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
